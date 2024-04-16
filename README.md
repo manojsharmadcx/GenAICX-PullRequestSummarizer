3c429bbecf9e9b8b46de4e9db2f55e715e0122ab
jayS202 <63660559+jayS202@users.noreply.github.com>
2024-04-16 15:10:49 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,201 @@
+# Comment Creator using ChatGPT
+
+This repository contains a Github action that generates explanations of changes using the OpenAI API as a comment in pull request. It analyzes the differences between two commits in a pull request and provides a summary of the changes.
+
+# Notable Features
+- A `token limit` for both `prompt` and `response` can be set to control costs. In the event that the token limit is reached a comment will not be created. The default limit is `10000` prompt tokens and `512` response tokens. For PRs with lots of changes, this limit will most likely need to be increased.
+- Prompts can be segmented into `multipe requests` to OpenAIApi and collated into a single response by setting the input `segment-size`, defaults to 3096.
+- The comment will be generated based on the `Pull Request Title` and a comparison of the file changes between the `Head SHA` of the pull request branch and the `Base SHA` (I.E Main branch).
+- A `custom prompt` can be configured allowing for flexibiliy in the response from ChatGPT. This can be configured via the input `custom-prompt`. The default value is `Combining each part. Summarize the changes in 300 words or less based on the pull request title and file changes.`.
+- Specific files and paths can be ignored when generating the changes by configuring the input `ignore-paths`.
+- The model used can be configured by setting the input `model`. The default model is `gpt-3.5-turbo`
+
+# API UML diagram
+
+![image](https://github.com/ps-aartread-org/chatgpt-comment-pull-request/assets/56265677/4aeef9d6-7284-409d-9ef9-cbc142d1c3de)
+
+
+## Prerequisites
+
+Before running this, ensure you have the following:
+
+- An OpenAI API key.
+- A GitHub personal access token.
+
+
+## Usage
+
+```yml
+      - name: ChatGpt Comment
+        uses: PublicisSapient/devops-chatgpt-comment-pull-request@main
+```
+
+This action will retrieve the pull request information and generate an explanation of the changes using the OpenAI API. If the explanation exceeds the maximum token limit or there are no changes after filtering, a comment will not be created.
+
+## Action inputs
+
+| Name | Description | Default | Required |
+| --- | --- | --- | --- |
+| `custom-prompt` | The prompt to feed to ChatGPT | Combining each part. Summarize the changes in 300 words or less based on the pull request title and file changes. | false |
+| `frequency-penalty` | Reduces the probability of words that have already been generated | `0` | false |
+| `github-token` | `GITHUB_TOKEN` (permissions `contents: write` and `pull-requests: write`) or a `repo` scoped [Personal Access Token (PAT)](https://docs.github.com/en/github/authenticating-to-github/creating-a-personal-access-token). | `GITHUB_TOKEN` | true |
+| `ignore-paths` | Comma separated list of paths and files those needs to be ignored from explanation | `All files are scanned if nothing is provided` | false |
+| `max-prompt-tokens` | The max-prompt-tokens variable is used to limit the number of tokens that are sent to OpenAI when generating an explanation of the changes in a pull request. The default value of 10000 is used. | `10000` | false |
+| `max-response-tokens` | The number of tokens allowed in the response | `512` | false |
+| `model` | The model to use for the AI | `gpt-3.5-turbo` | false |
+| `open-api-key` | OPENAI API Token created from https://platform.openai.com/account/api-keys. | `CHATGPT_API_KEY` | true |
+| `response-penalty` | Reduces the probability of a word if it already appeared in the predicted text |     `0` | false |
+| `segment-size` | The number of tokens used to segment the prompt | `3096` | false |
+| `temperature` | Parameter that controls how much randomness is in the input | `1` | false |
+| `top_p` | Controls how many of the highest-probability words are selected to be included in the generated text | `1` | false |
+
+## Configuration
+
+Action provides several configuration options that you can modify based on your requirements:
+
+- `apiKey`: Your OpenAI API key.
+- `github-token`: Your GitHub personal access token.
+- `max-prompt-tokens`: The maximum number of tokens allowed in the prompt.
+- `ignore-paths`: Comma-separated list of file paths or patterns to ignore.
+- `model`: The OpenAI language model to use for generating explanations.
+- `temerature`: Parameter that controls how much randomness is in the output.
+- `max-response-tokens`: The maximum number of tokens allowed in the response.
+- `top_p`: Controls how many of the highest-probability words are selected to be included in the generated text.
+- `frequency-penalty`: Reduces the probability of words that have already been generated.
+- `presence-penalty`: Reduces the probability of a word if it already appeared in the predicted text.
+- `segment-size`: The number of tokens used to segment the prompt.
+
+## Action.yml
+
+The `action.yml` file contains the metadata for the action.
+
+```yaml
+name: 'ChatGPT Comment'
+description: 'Autogenerated a PR comment based on the code changes'
+inputs:
+  custom-prompt:
+    description: 'Prompt to feed to ChatGPT'
+    required: false
+    default: 'Given all the parts. Summarize the changes in 300 words or less'
+  frequency-penalty:
+    description: 'Reduces the probability of words that have already been generated'
+    required: false
+    default: 0
+  github-token:
+    description: 'GitHub Token'
+    required: true
+  ignore-paths:
+    description: 'comma separated list of paths and files'
+    required: false
+  max-prompt-tokens:
+    description: 'The maximum number of tokens to use for the prompt'
+    required: false
+    default: '10000'
+  max-response-tokens:
+    description: 'The maximum number of tokens allowed in the response'
+    required: false
+    default: 512
+  model: 
+    description: 'The model to use for the AI'
+    required: false
+    default: 'gpt-3.5-turbo'
+  open-api-key:
+    description: 'OpenAPI Token'
+    required: true
+  presence-penalty:
+    description: 'Reduces the probability of a word if it already appeared in the predicted text'
+    required: false
+    default: 0
+  segment-size:
+    description: 'The number of tokens used to segment the prompt'
+    required: false
+    default: 3096
+  temperature:
+    description: 'Parameter that controls how much randomness is in the output'
+    required: false
+    default: 1
+  top_p:
+    description: 'Controls how many of the highest-probability words are selected to be included in the generated text'
+    required: false
+    default: 1
+runs:
+  using: 'node16'
+  main: 'dist/index.js'
+```
+
+## Index.js
+
+The `index.js` file contains the main logic for the action.
+
+```javascript
+// Import required packages and libraries
+const axios = require('axios');
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { encode, decode } = require('gpt-3-encoder')
+
+const { Configuration, OpenAIApi } = require("openai");
+const { Octokit } = require('@octokit/rest');
+const { context: githubContext } = require('@actions/github');
+
+// ... (rest of the code)
+```
+
+## Reference example
+
+The following workflow sets many of the action's inputs for reference purposes.
+Check the [defaults](#action-inputs) to avoid setting inputs unnecessarily.
+
+See below for the use cases.
+
+```yml
+on:
+  pull_request:
+    branches:
+      - main
+  workflow_dispatch:
+
+jobs:
+  chatgptComment:
+    runs-on: ubuntu-latest
+    name: Add Comment
+    steps:
+      - name: Add Comment
+        uses: PublicisSapient/devops-chatgpt-comment-pull-request@main
+        with:
+          github-token: ${{ secrets.GITHUB_TOKEN }}
+          open-api-key: ${{ secrets.CHATGPT_API_KEY }}
+          max-prompt-tokens: '10000'
+          ignore-paths: '.github/*, src/, package*.json, .env*'
+          model: 'gpt-3.5-turbo'
+          temperature: 1
+          max-response-tokens: 512
+          top_p: 1
+          frequency-penalty: 0
+          presence-penalty: 1
+
+```
+
+An example based on the above reference configuration adds comment that look like this:
+
+<img width="961" alt="image" src="https://github.com/ps-aartread-org/chatgpt-comment-pull-request/assets/56265677/729f6d2b-3d2c-4549-b37b-fbb8c411aec4">
+
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+## Contributions
+
+Contributions to this project are welcome. Feel free to open issues or submit pull requests to improve the script.
+
+## Credits
+
+This script utilizes the following packages and libraries:
+
+- axios
+- @actions/core
+- @actions/github
+- gpt-3-encoder
+- openai
+- @octokit/rest
---
+++ index.js
@@ -0,0 +1,240 @@
+// Import required packages and libraries
+const axios = require('axios');
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { encode, decode } = require('gpt-3-encoder')
+
+// const { Configuration, OpenAIApi } = require("openai");
+const { OpenAIClient, AzureKeyCredential } = require("@azure/openai");
+
+const { Octokit } = require('@octokit/rest');
+const { context: githubContext } = require('@actions/github');
+
+// Create an OpenAI instance using the provided API key
+// const configuration = new Configuration({
+//   apiKey: core.getInput('open-api-key')
+// });
+// const openai = new OpenAIApi(configuration);
+
+const endpoint = core.getInput('azure-endpoint');
+const azureApiKey = core.getInput('azure-api-key');
+
+// Function to generate the explanation of the changes using OpenAI API
+async function generateExplanation(changes) {
+  const client = new OpenAIClient(endpoint, new AzureKeyCredential(azureApiKey));
+  const deploymentId = "gpt-35-turbo";
+
+
+  const encodedDiff = encode(JSON.stringify(changes));
+  const totalTokens = encode(JSON.stringify(changes)).length;
+
+  let model = core.getInput('model');
+  let temperature = parseInt(core.getInput('temperature'));
+  let maxResponseTokens = parseInt(core.getInput('max-response-tokens'));
+  let topP = parseInt(core.getInput('top_p'));
+  let frequencyPenalty = parseInt(core.getInput('frequency-penalty'));
+  let presencePenalty = parseInt(core.getInput('presence-penalty'));
+  let segmentSize = parseInt(core.getInput('segment-size'));
+
+  console.log('model = '+ model);
+  console.log('temperature = '+ temperature);
+  console.log('max_tokens = '+maxResponseTokens);
+  console.log('top_p = '+ topP);
+  console.log('frequency_penalty = '+ frequencyPenalty);
+  console.log('presence_penalty = '+ presencePenalty);
+  console.log('Segment Size = '+ segmentSize);
+
+  // Function to split the incoming changes into smaller chunks.
+  function splitStringIntoSegments(encodedDiff, totalTokens, segmentSize) {
+    const segments = [];
+
+    for (let i = 0; i < totalTokens; i += segmentSize) {
+      segments.push(encodedDiff.slice(i, i + segmentSize));
+    }
+    return segments;
+  }
+
+  const segments = splitStringIntoSegments(encodedDiff, totalTokens, segmentSize);
+
+  // Loop through each segment and send the request to OpenAI.
+  // If the segment is not the last segment, just receive and acknowledge. Otherwise, return the response.
+  for (let i = 0; i < segments.length; i++) {
+    let obj = decode(segments[i]);
+    let part = i + 1;
+    let totalParts = segments.length;
+    console.log('Segment Tokens:', encode(JSON.stringify(obj)).length);
+    console.log(`This is part ${part} of ${totalParts}`);
+
+    if (part != totalParts) {
+      let prompt = `This is part ${part} of ${totalParts}. Just receive and acknowledge as Part ${part}/${totalParts} \n\n${obj}`;
+      console.log(prompt);
+
+      const messages = [
+        {role: "user", content: prompt }
+      ];
+
+      await client.getChatCompletions(deploymentId,messages,{
+        max_tokens: maxResponseTokens
+      });
+    } else {
+      let customPrompt = core.getInput('custom-prompt');
+      let prompt = `This is part ${part} of ${totalParts}. ${customPrompt}\n\n${obj}`;
+      console.log(prompt);
+
+      const messages = [
+        {role: "user", content: prompt }
+      ];
+
+      let response = await client.getChatCompletions(deploymentId,messages,{
+        max_tokens: maxResponseTokens
+      });
+
+      const explanation = response.choices[0].message.content.trim();
+      // const explanation = response.data.choices[0].message.content.trim();
+      return explanation;
+    }
+  }
+}
+
+try {
+  // Get the PR number, repository, and token from the GitHub webhook payload
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  const jsonData = JSON.parse(payload);
+  const pullRequestNumber = jsonData.number;
+  const pullRequestTitle = jsonData.pull_request.title;
+  const repository = jsonData.pull_request.base.repo.full_name;
+  const token = core.getInput('github-token');
+
+  // Retrieve the base and head commit information for the pull request
+  const pullRequestUrl = `https://api.github.com/repos/${repository}/pulls/${pullRequestNumber}`;
+  const headers = {
+    Accept: 'application/vnd.github.v3+json',
+    Authorization: `Bearer ${token}`,
+  };
+
+  axios.get(pullRequestUrl, { headers: headers })
+    .then((response) => {
+
+      // Set Base and Head CommitIDs
+      const pullRequestData = response.data;
+
+      return pullRequestData
+
+    })
+    .then((pullRequestData) => {
+      // const numComments = pullRequestData.comments;
+      const headCommitSha = pullRequestData.head.sha;
+      const baseCommitSha = pullRequestData.base.sha;
+
+      return Promise.all([
+        headCommitSha,
+        baseCommitSha,
+      ])
+
+    })
+    .then(([headCommitSha, baseCommitSha]) => {
+      console.log('Head Commit:', headCommitSha);
+      console.log('Base Commit:', baseCommitSha);
+
+      // Retrieve the file changes between the base and head commits
+      const commitUrl = `https://api.github.com/repos/${repository}/commits/`;
+      const baseCommitUrl = commitUrl + baseCommitSha;
+      const headCommitUrl = commitUrl + headCommitSha;
+
+      return Promise.all([
+        axios.get(baseCommitUrl, { headers: headers }),
+        axios.get(headCommitUrl, { headers: headers }),
+      ]);
+
+    })
+    .then(([baseCommitResponse, headCommitResponse]) => {
+      // Compare the Commit IDs and get a back response in JSON.
+      const baseCommitData = baseCommitResponse.data;
+      const headCommitData = headCommitResponse.data;
+
+      // Retrieve the diff and changes between the base and head commits
+      const compareUrl = `https://api.github.com/repos/${repository}/compare/${baseCommitData.sha}...${headCommitData.sha}`;
+      return axios.get(compareUrl, { headers: headers });
+    })
+    .then((compareResponse) => {
+      // Get the data and output the file changes.
+      const compareData = compareResponse.data;
+      const fileChanges = compareData.files;
+
+      let ignorePathsInput = core.getInput('ignore-paths');
+      let ignorePaths = [];
+      if (ignorePathsInput) {
+        ignorePaths = ignorePathsInput.split(',');
+      }
+
+      // Function to check if a file or path should be ignored
+      function shouldIgnore(path) {
+        return ignorePaths.some(ignorePath => {
+          const trimmedIgnorePath = ignorePath.trim();
+
+          if (trimmedIgnorePath.endsWith('/')) {
+            // Directory path
+            return path.startsWith(trimmedIgnorePath);
+          } else if (trimmedIgnorePath.endsWith('*')) {
+            // Wildcard file name
+            const prefix = trimmedIgnorePath.slice(0, -1);
+            return path.startsWith(prefix);
+          } else {
+            // Literal file name
+            return path === trimmedIgnorePath;
+          }
+        });
+      }
+
+      // Filter out ignored files and paths
+      const filteredChanges = JSON.stringify(fileChanges.filter(change => !shouldIgnore(change.filename)));
+      const changes = (`Pull Request Title: ${pullRequestTitle}\n\nFile Changes: ${filteredChanges}` );
+
+      // Calculate the token count of the prompt
+      const tokens = encode(JSON.stringify(changes)).length;
+      const maxPromptTokens = core.getInput('max-prompt-tokens'); // Maximum prompt tokens allowed
+
+      // Print Prompt Token Count & Max Prompt Tokens
+      console.log('Prompt Token Count:', tokens);
+      console.log('Max Prompt Tokens: ', maxPromptTokens);
+
+      if (tokens > maxPromptTokens || (ignorePathsInput && filteredChanges.length === 0)) {
+        console.log('Skipping Comment due to Max Tokens or No Changes after Filtering');
+        const explanation = 'skipping comment';
+        return explanation;
+      } else {
+        return generateExplanation(changes);
+      }
+    })
+    .then((explanation) => {
+      // Create the GitHub Comment
+      console.log(explanation.split('-').join('\n'));
+
+      // Create a comment with the generated explanation
+      const octokit = new Octokit({ auth: token });
+      const comment = `Explanation of Changes (Generated via OpenAI):\n\n${JSON.stringify(explanation)}`;
+
+      async function createComment() {
+        const newComment = await octokit.issues.createComment({
+          ...githubContext.repo,
+          issue_number: githubContext.issue.number,
+          body: comment
+        });
+
+        console.log(`Comment added: ${newComment.data.html_url}`);
+      }
+
+      // Create Comment if Explanation does not contain 'skipping comment' due to max tokens limit
+      if (explanation == 'skipping comment') {
+        console.log('Skipping Comment due to Max Tokens or No Changes after Filtering');
+      } else {
+        createComment();
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+} catch (error) {
+  core.setFailed(error.message);
+}
